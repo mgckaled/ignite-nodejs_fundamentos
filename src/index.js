c9a4f6d7f35ff4ecbe834629ce9d8cde0b211f98
mgckaled <marcelgckaled@gmail.com>
2022-11-15 18:03:36 -0300

    index.js done

--- src/index.js
+++ src/index.js
@@ -2,20 +2,28 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 // request -> recebe da requisição
 // response -> retorna para a requisição
 app.get('/', (request, response) => {
 	return response.json({ message: 'Hello World Ignite - Fundamentos NodeJS' })
 })
 app.get('/courses', (request, response) => {
+	const query = request.query
+	console.log(query)
 	return response.json(['Curso 1', 'Curso 2', 'Curso 3'])
 })
 
 app.post('/courses', (request, response) => {
+	const body = request.body
+	console.log(body)
 	return response.json(['Curso 1', 'Curso 2', 'Curso 3', 'Curso 4'])
 })
 
 app.put('/courses/:id', (request, response) => {
+	const { id } = request.params
+	console.log(id)
 	return response.json(['Curso 6', 'Curso 2', 'Curso 3', 'Curso 4'])
 })
 
